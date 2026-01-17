9e44ed5b366f91a0e9f7eb7ecabc9311cd80cc75
yuan yuan <fuyuanlum@gmail.com>
2026-01-17 16:41:58 +0800

    vscode

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo hihi2',
+      title: 'Flutter Demo hihi2 from vscode',
       theme: ThemeData(
         // This is the theme of your application.
         //
